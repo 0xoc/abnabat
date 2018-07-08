161f8109d947f260d1ee137bf8c7d79935d437cc
Sina Parvizi <snparvizi75@gmail.com>
2018-07-08 07:16:43 +0430

    Basic Inventory models added

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from inventory.models import *
 # Register your models here.
+
+admin.site.register(Field)
+admin.site.register(Product)
+admin.site.register(PIR)
+admin.site.register(Inventory)
--- inventory/models.py
+++ inventory/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Field(models.Model):
+    key = models.CharField(max_length = 255)
+    value = models.CharField(max_length = 1200)
+    field_type = models.CharField(max_length = 255)
+    def __str__(self):
+        return self.key + "->" + self.value
 class Product(models.Model):
-    pass
-
+    name = models.CharField(max_length = 255)
+    fields = models.ManyToManyField(Field) #:)
+    def __str__(self):
+        return self.name
+class PIR(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name="Origin")
+    count = models.IntegerField()
+    price = models.IntegerField()
 class Inventory(models.Model):
-    pass
+    products = models.ManyToManyField(PIR,related_name="Inventory");
