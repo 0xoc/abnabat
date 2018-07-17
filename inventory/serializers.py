8ee4dbb7fc882c63e8374b896c6218401bbf7c37
Sina Parvizi <snparvizi75@gmail.com>
2018-07-17 20:05:35 +0430

    Product Serializer supports nested update (put/path)

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -8,7 +8,7 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
     fields = FieldSerializer(many=True)
     class Meta:
         model = Product
-        fields = ['name','fields','price']
+        fields = ['id','name','fields','price']
     def create(self,data):
         fields = data.pop('fields')
         product = Product.objects.create(**data)
@@ -16,6 +16,21 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
             newField = Field.objects.create(**field)
             product.fields.add(newField)
         return product
+    def update(self,instance,data):
+        instance.name = data.get('name',instance.name)
+        instance.price = data.get('price',instance.price)
+        fields = data.pop('fields')
+        i = 0
+        for field in instance.fields.all():
+            field.key = fields[i].get('key',field.key)
+            field.value = fields[i].get('value',field.value)
+            field.field_type = fields[i].get('field_type',field.field_type)
+            field.save()
+            i += 1
+        instance.save()
+        return instance
+
+
 class PIRSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = PIR
