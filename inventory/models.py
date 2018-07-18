ec86317d7337e27c2d2b9acefc99ec551da07e8c
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-07-19 00:21:32 +0430

    moved product things to shop app | renamed Field to ProductMeta for avoiding conflicts with other classes

--- inventory/models.py
+++ inventory/models.py
@@ -1,23 +1,5 @@
 from django.db import models
 import os
-# Create your models here.
-class Field(models.Model):
-    key = models.CharField(max_length = 255)
-    value = models.CharField(max_length = 1200)
-    field_type = models.CharField(max_length = 255)
-    def __str__(self):
-        return self.key + "->" + self.value
-class ImageField(models.Model):
-    img = models.ImageField(upload_to="%y/%m/%d/%s")
-
-    def __str__(self):
-        return os.path.basename(self.img.name)
-class Product(models.Model):
-    name = models.CharField(max_length = 255)
-    fields = models.ManyToManyField(Field) #:)
-    price = models.IntegerField()
-    def __str__(self):
-        return self.name
 
 class PIR(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name="Origin")
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import os
 
-# Create your models here.
+
+class ProductMeta(models.Model):
+    key = models.CharField(max_length = 255)
+    value = models.CharField(max_length = 1200)
+    field_type = models.CharField(max_length = 255)
+    def __str__(self):
+        return self.key + "->" + self.value
+
+class ImageField(models.Model):
+    img = models.ImageField(upload_to="%y/%m/%d/%s")
+
+    def __str__(self):
+        return os.path.basename(self.img.name)
+
+class Product(models.Model):
+    name = models.CharField(max_length = 255)
+    fields = models.ManyToManyField(ProductMeta) #:)
+    price = models.IntegerField()
+    def __str__(self):
+        return self.name
+
