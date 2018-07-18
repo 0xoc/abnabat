c76e18048d736aced8a99d16089868b88ec79ddf
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-07-19 00:48:02 +0430

    fixed product and meta relations

--- shop/models.py
+++ shop/models.py
@@ -2,7 +2,14 @@ from django.db import models
 import os
 
 
+class Product(models.Model):
+    name = models.CharField(max_length = 255)
+    price = models.IntegerField()
+    def __str__(self):
+        return self.name
+
 class ProductMeta(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name="Origin")
     key = models.CharField(max_length = 255)
     value = models.CharField(max_length = 1200)
     field_type = models.CharField(max_length = 255)
@@ -10,15 +17,9 @@ class ProductMeta(models.Model):
         return self.key + "->" + self.value
 
 class ProductImageMeta(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name="Origin")
     img = models.ImageField(upload_to="%y/%m/%d/%s")
 
     def __str__(self):
         return os.path.basename(self.img.name)
-
-class Product(models.Model):
-    name = models.CharField(max_length = 255)
-    fields = models.ManyToManyField(ProductMeta) #:)
-    price = models.IntegerField()
-    def __str__(self):
-        return self.name
 
