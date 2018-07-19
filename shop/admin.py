e02f47f00dc05f69001f91ceb09aaa0d599c99cd
Sina Parvizi <snparvizi75@gmail.com>
2018-07-19 23:07:32 +0430

    changed ImageField to ProductImageMeta

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from shop.models import Product, ImageField, ProductMeta
+from shop.models import Product, ProductImageMeta, ProductMeta
 
 admin.site.register(Product)
 admin.site.register(ProductMeta)
-admin.site.register(ImageField)
+admin.site.register(ProductImageMeta)
