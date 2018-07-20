48bf563a525c07564096e8519f4db33f9c8c23b0
Sina Parvizi <snparvizi75@gmail.com>
2018-07-20 12:38:06 +0430

    Product serializers supports nested image

--- shop/serializers.py
+++ shop/serializers.py
@@ -16,14 +16,24 @@ class ProductImageMetaSerializer(serializers.HyperlinkedModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     CharFields = ProductMetaSerializer(many=True)
-
+    ImageFields = ProductImageMetaSerializer(many=True)
     class Meta:
         model = Product
-        fields = ['id', 'name', 'CharFields', 'price']
+        fields = ['id', 'name', 'price','CharFields','ImageFields']
 
     def create(self, data):
         char_fields = data.pop('CharFields')
+        image_fields = data.pop('ImageFields')
+
+        # create raw prodcut
         product = Product.objects.create(**data)
+
+        # Create char fields
         for char_field in char_fields:
             new_char_field = ProductMeta.objects.create(**char_field,product=product)
+
+        # create Image Fileds
+        for img_field in image_fields:
+            new_img_filed = ProductImageMeta.objects.create(**img_field,product=product)
+        
         return product
